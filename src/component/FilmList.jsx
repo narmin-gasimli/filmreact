07ebf5bc252bb00971a20cf992d55100773705ef
narmin-gasimli <nerminqasimli03@gmail.com>
2023-12-10 23:20:52 +0400

    second commit

---
+++ src/component/FilmList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+const FilmList = (props) => {
+    const FavouriteComponent = props.favouriteComponent;
+    
+   return (
+    <>
+        {props.movies.map((movie, index) => (
+        <div className="image-container ">
+            <img src={movie.Poster} alt='movie'></img>
+            <div 
+            onClick={() => props.handleFavouritesClick(movie)} 
+            className="overlay" >
+                <FavouriteComponent />
+            </div>
+        </div>
+        ))}
+    </>
+   ); 
+};
+
+
+export default FilmList;
